d0244a397335edcf041a4a75caa6448ac7fcb662
Anisoara-Ionela Plesca <anisoara.ionela@gmail.com>
2020-10-27 18:55:56 +0200

    test not working

--- social/src/app/app.component.spec.ts
+++ social/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('social');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('social app is running!');
-  // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('social app is running!');
+  });
 });
