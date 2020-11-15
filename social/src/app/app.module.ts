f137fe863cc85590f4a7a54775c1222be4d1761b
nela <anisoara.ionela@gmail.com>
2020-11-15 20:45:38 +0200

    add sso providers

--- social/src/app/app.module.ts
+++ social/src/app/app.module.ts
@@ -4,17 +4,44 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoglComponent } from './components/gogl/gogl.component';
+import { gClientId, fbClientId } from './secrets';
+
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+} from 'angularx-social-login';
+
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  VKLoginProvider,
+} from 'angularx-social-login';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GoglComponent
+  declarations: [AppComponent, GoglComponent],
+  imports: [BrowserModule, AppRoutingModule, SocialLoginModule],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(gClientId),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider(fbClientId),
+          },
+          {
+            id: VKLoginProvider.PROVIDER_ID,
+            provider: new VKLoginProvider('7624815'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- social/src/app/components/gogl/gogl.component.ts
+++ social/src/app/components/gogl/gogl.component.ts
@@ -1,30 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import {gClientId} from '../../secrets'
+
+import { SocialAuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+  VKLoginProvider,
+} from 'angularx-social-login';
 
 @Component({
   selector: 'app-gogl',
   templateUrl: './gogl.component.html',
-  styleUrls: ['./gogl.component.css']
+  styleUrls: ['./gogl.component.css'],
 })
 export class GoglComponent implements OnInit {
+  user: SocialUser;
 
-  constructor() { }
-  public gClientId = gClientId;
-  
+  constructor(private authService: SocialAuthService) {}
 
-  ngOnInit(): void {
-  };
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
 
-  onSignIn(googleUser) {
-    console.log('sin clikd')
-    var profile = googleUser.getBasicProfile();
-    // console
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  };
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
 
-  
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
 
+  signInWithVK(): void {
+    this.authService.signIn(VKLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
 }
